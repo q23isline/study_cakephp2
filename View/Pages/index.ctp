c9a2416866b9e329101c4f5800cdf794e31e1009
q23isline <rowpure@gmail.com>
2023-10-07 18:31:40 +0900

    VueとCakePHPを連携させる

---
+++ View/Pages/index.ctp
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var \View $this
+ */
+?>
+<div id="app">
+</div>
+<?= $this->Html->script('/assets/js/about.js') ?>
+<?= $this->Html->script('/assets/js/chunk-vendors.js') ?>
+<?= $this->Html->script('/assets/js/index.js') ?>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,22 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ * @link https://cli.vuejs.org/config/
+ */
 module.exports = {
     pages: {
         index: {
             entry: 'src/main.ts'
         }
     },
-    outputDir: '../webroot/assets'
+    // ビルド後にできるファイルを webroot/assets に指定すると img 等のパスがズレて読み込めなくなるので webroot にする
+    // （ビルド時に webroot 全消しを防ぐために --no-clean オプションをつけておく）
+    // outputDir: '../webroot/assets',
+    outputDir: '../webroot',
+    // ビルド後にできるファイルは webroot の assets に置く（ただ frontend/public に置いたものは webroot にできる）
+    assetsDir: 'assets',
+    indexPath: 'assets/index.html',
+    // Vue で生成された URL にまで assets が出てくるようになるので指定しない
+    // publicPath: 'assets',
+    // ビルド後にできるファイル名にハッシュ値つけない（バックエンドで固定値で呼びだせるようにする）
+    filenameHashing: false
 }
