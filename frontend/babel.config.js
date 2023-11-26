bfe6ef458901bec5a99e33a1c418293a0469e9ed
q23isline <rowpure@gmail.com>
2023-11-26 22:45:19 +0900

    設定ファイルの型読み込み

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,7 @@
+/**
+ * @type {import('@babel/core').TransformOptions}
+ * @link https://babeljs.io/docs/configuration#javascript-configuration-files
+ */
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
 };
